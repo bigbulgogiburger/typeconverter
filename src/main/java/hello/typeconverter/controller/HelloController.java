3374ec3d4ac4254e6977ac10dad376e8453da295
dohoon pyun <77317742+bigbulgogiburger@users.noreply.github.com>
2023-03-12 03:42:04 +0900

    added

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,14 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV1(@RequestParam Integer data){
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort){
+        System.out.println("IpPort = " + ipPort.getPort());
+        System.out.println("IpPort = " + ipPort.getIp());
+
         return "ok";
     }
 }
